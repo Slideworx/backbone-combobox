ad08c2679ad4dccb211aa7f6d831fe89225f1ca1
Michał Świtoń <michal.switon@slideworx.com>
2016-06-02 15:33:41 +0200

    wrapped js files with define block (require)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-module.exports = {
-  View: require('./src/comboboxView'),
-  Model: require('./src/comboboxModel'),
-  DropdownView: require('./src/dropdownView')
-};
+define(function(require, exports, module) {
+  module.exports = {
+    View: require('./src/comboboxView'),
+    Model: require('./src/comboboxModel'),
+    DropdownView: require('./src/dropdownView')
+  };
+});
